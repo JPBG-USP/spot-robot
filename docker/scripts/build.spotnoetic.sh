5c4d8c1cb70d3002c7f758cfe44131d7993a6dd8
João Pedro <joao_garciajp@usp.br>
2024-02-20 14:20:10 -0300

    Adding Rviz in docker

--- docker/scripts/build.spotnoetic.sh
+++ docker/scripts/build.spotnoetic.sh
@@ -1,1 +1,14 @@
-# empty yet
+# Here we have the script that build our spotnoetic
+
+# Check if you are on the right directory to run the build
+if [[ $PWD = *spot-robot ]]; then
+    cd docker
+elif [[ ! $PWD = *spot-robot/docker ]]; then
+    echo -e "You must be in either 'spot-robot' or the 'spot-robot/docker' directory to run this command."
+    return 1
+fi
+
+docker build -f Dockerfile.spotnoetic\
+    -t spot-robot:spotnoetic\
+    --rm\
+    .
--- docker/scripts/run.spotnoetic.sh
+++ docker/scripts/run.spotnoetic.sh
@@ -1,2 +1,3 @@
 # Here you will find the commands to have a good ininicialization of docker
-docker run -it --user ros --network=host my-ros-noetic
+# docker run -it --user ros --network=host --ipc=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e DISPLAY=$DISPLAY spot-robot:spotnoetic
+docker run -it --user ros --network=host --ipc=host -v /dev/dri:/dev/dri -e DISPLAY=$DISPLAY spot-robot:spotnoetic
