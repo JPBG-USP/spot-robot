98fa82fa4531ae1a7ea5df91aafa8bcb39db782c
João Pedro <joao_garciajp@usp.br>
2024-02-23 22:52:26 -0300

    Minor changes in documentations

--- docker/scripts/build.spotnoetic.sh
+++ docker/scripts/build.spotnoetic.sh
@@ -5,8 +5,14 @@ if [[ $PWD = *spot-robot ]]; then
     cd docker
 elif [[ ! $PWD = *spot-robot/docker ]]; then
     echo -e "You must be in either 'spot-robot' or the 'spot-robot/docker' directory to run this command."
+    echo -e "Você precisa estar na pasta 'spot-robot' ou em 'spot-robot/docker' para rodar esse commando."
     return 1
 fi
+
+# Configs da build
+# -f Dockerfile.spotnoetic  (-f ou --file informa o arquivo a ser dado o build)
+# -t spot-robot:spotnoetic  (-t ou --tag é para o repositório e para a tag respectivamente, separados por dois pontos)
+# --rm (Ouvi que é bom deixar, acho que remove as versões de builds antigas)
 
 docker build \
     --network=host \
--- docker/scripts/run.spotnoetic.sh
+++ docker/scripts/run.spotnoetic.sh
@@ -1,8 +1,32 @@
-# Here you will find the commands to have a good ininicialization of docker
-# docker run -it --user ros --network=host --ipc=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e DISPLAY=$DISPLAY spot-robot:spotnoetic
+# Here you will find the commands to inicialize the docker image the rigth way
+# Aqui você vai encontrar os comandos para inicializar a imagem docker do jeito bom
+
 if [[ $PWD = *spot-robot ]]; then
-    docker run -it --user ros --network=host --ipc=host -v /dev/dri:/dev/dri -v $PWD/ros_ws:/home/ros/ros_ws:rw -e DISPLAY=$DISPLAY spot-robot:spotnoetic
+
+    #                       Configs para inicialização do container
+    #                                -it    (Terminal interativo)
+    #                             --user    (Setando qual usuário do container vc vai usar)
+    #        --network=host e --ipc=host    (Pra ter acesso a internet)
+    #               -v /dev/dri:/dev/dri    (Nesse caso to dando acesso a todas as pastas de driver, assim ele tem acesso ao driver de video do meu PC)
+    #    -v $PWD/ros_ws:/home/ros/ros_ws    (Pra ter acesso a pasta de ROS do robo)
+    #                -e DISPLAY=$DISPLAY    (Setando pra aparecer no display que estamos usando atualmente)
+    #              spot-robot:spotnoetic    (A imagem docker que queremos)
+
+    docker run -it \
+    --user ros \
+    --network=host \
+    --ipc=host \
+    -v /dev/dri:/dev/dri \
+    -v $PWD/ros_ws:/home/ros/ros_ws \
+    -e DISPLAY=$DISPLAY \
+    spot-robot:spotnoetic
+
 elif [[ ! $PWD = *spot-robot/docker ]]; then
-    echo -e "You must be in either 'spot-robot' directory to run this command."
+    echo -e "You must be in 'spot-robot' directory to run this command."
+    echo -e "Você deve estar na pasta 'spot-robot' para rodar esse comando."
     return 1
 fi
+
+# Aqui abaixo tem um comando pra quem tem placa da Nvidia eu acho, não funcionou no meu pq eu não uso esse X11-unix (minha teoria), mas
+# você pode tentar.
+# docker run -it --user ros --network=host --ipc=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e DISPLAY=$DISPLAY spot-robot:spotnoetic
